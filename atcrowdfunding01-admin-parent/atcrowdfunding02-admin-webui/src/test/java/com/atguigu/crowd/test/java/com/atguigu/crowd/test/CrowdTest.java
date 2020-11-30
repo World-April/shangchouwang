26d6406103aacb34e0d935e454f70c6250239b6f
Axianyo <1394019338@qq.com>
2020-11-30 23:47:15 +0800

    的

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/test/java/com/atguigu/crowd/test/java/com/atguigu/crowd/test/CrowdTest.java
@@ -0,0 +1,57 @@
+package com.atguigu.crowd.test.java.com.atguigu.crowd.test;
+
+
+import com.atguigu.crowd.mapper.AdminMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+// 在类上标记必要的注解，Spring整合Junit
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-persist-mybatis.xml"})
+public class CrowdTest {
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    AdminMapper adminMapper;
+
+    @Test
+    public void testLog() {
+
+        // 1.获取Logger对象，这里传入的Class对象就是当前打印日志的类
+        Logger logger = LoggerFactory.getLogger(CrowdTest.class);
+
+        // 2.根据不同日志级别打印日志
+        logger.debug("Hello I am Debug level!!!");
+        logger.debug("Hello I am Debug level!!!");
+        logger.debug("Hello I am Debug level!!!");
+
+        logger.info("Info level!!!");
+        logger.info("Info level!!!");
+        logger.info("Info level!!!");
+
+        logger.warn("Warn level!!!");
+        logger.warn("Warn level!!!");
+        logger.warn("Warn level!!!");
+
+        logger.error("Error level!!!");
+        logger.error("Error level!!!");
+        logger.error("Error level!!!");
+    }
+
+    @Test
+    public void testConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+    }
+
+}
